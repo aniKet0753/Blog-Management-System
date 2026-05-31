dbc634929436b05b3a8f54e9d44775262dc5833a
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-05-31 19:54:51 +0530

    Change API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://blog-management-system-x7wu.onrender.com/api",
 });
 
 export default api;
